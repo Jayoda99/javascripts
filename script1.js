50ac8e6d92ef59a38eed8f87747805f148c5aeaf
Jayoda99 <138502443+Jayoda99@users.noreply.github.com>
2023-11-04 14:57:20 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,31 @@
+console.log('Hi from script.js file');
+
+var firstname = 'Nimal',
+	age = 25,
+	married = false,
+	middleName = null,
+	lastName;
+
+var cars = ["Toyota","Nissan","Honda",30,true]
+cars[2]
+cars[0]
+var motorBikes = [];
+var airPlanes = new Array("AirBus","Being");
+airPlanes[0]
+airPlanes[1]
+cars.length
+motorBikes.length
+cars.push("Suzuki")
+motorBikes.push("Honda")
+cars.pop()
+
+
+heights = [4.9,5.2,7]
+heights.push('8','6')
+heights.pop()
+heights[3]
+heights.shift()
+heights.unshift("Toyota")
+heights.indexOf("Nissan")
+heights.indexOf(7)
+heights.indexOf('8')
